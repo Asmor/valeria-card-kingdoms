36d1f44954477024f1d6566fccd1e45a53110c45
asmor <itoltz@gmail.com>
2017-08-23 22:20:52 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+	name: "app",
+	data () {
+		return {
+			msg: "Hello, world!",
+		};
+	},
+	components: {},
+};
+</script>
+
+<template>
+	<div id="app">
+		<h1>
+			Hello?
+		</h1>
+	</div>
+</template>
+
+<style>
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/main.js",
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+		publicPath: "dist/",
+		filename: "build.js",
+	},
+	module: {
+		rules: [
+			{
+			test: /\.vue$/,
+			loader: "vue-loader",
+			options: {
+				'scss': 'vue-style-loader!css-loader!sass-loader',
+				'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+			},
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]?[hash]'
+				}
+			},
+			{
+				test: /\.json$/,
+				loader: 'json-loader',
+				exclude: /node_modules/
+			},
+		],
+	},
+	resolve: {
+		alias: {
+			"vue$": "vue/dist/vue.esm.js",
+		},
+	},
+	devServer: {
+		historyApiFallback: true,
+		noInfo: true,
+	},
+	performance: {
+		hints: false,
+	},
+	devtool: "#eval-source-map",
+};
