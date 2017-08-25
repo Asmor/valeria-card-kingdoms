e0a04701835e4cce332a1aed5a4b237e7d9776ad
asmor <itoltz@gmail.com>
2017-08-24 20:39:00 -0400

    Added appcache, but not working; commented out for now

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var webpack = require("webpack");
+// var AppCachePlugin = require('appcache-webpack-plugin');
 
 module.exports = {
 	entry: "./src/main.js",
@@ -68,14 +69,70 @@ if (process.env.NODE_ENV === 'production') {
 				NODE_ENV: '"production"'
 			}
 		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false
-			}
-		}),
+		// new webpack.optimize.UglifyJsPlugin({
+		// 	sourceMap: true,
+		// 	compress: {
+		// 		warnings: false
+		// 	}
+		// }),
 		new webpack.LoaderOptionsPlugin({
 			minimize: true
-		})
+		}),
+		// new AppCachePlugin({
+		// 	cache: [
+		// 		"index.html",
+		// 		"manifest.json",
+		// 		"images/01-cleric.jpg",
+		// 		"images/01-monk.jpg",
+		// 		"images/01-summoner.jpg",
+		// 		"images/02-bard.jpg",
+		// 		"images/02-blacksmith.jpg",
+		// 		"images/02-merchant.jpg",
+		// 		"images/03-alchemist.jpg",
+		// 		"images/03-mercenary.jpg",
+		// 		"images/03-sorceress.jpg",
+		// 		"images/04-archer.jpg",
+		// 		"images/04-barbarian.jpg",
+		// 		"images/04-wizard.jpg",
+		// 		"images/05-alt-peasant.jpg",
+		// 		"images/05-peasant.jpg",
+		// 		"images/06-alt-knight.jpg",
+		// 		"images/06-knight.jpg",
+		// 		"images/07-08-kings-guard.jpg",
+		// 		"images/07-condottiere.jpg",
+		// 		"images/07-rogue.jpg",
+		// 		"images/07-thief.jpg",
+		// 		"images/08-bogatyr.jpg",
+		// 		"images/08-champion.jpg",
+		// 		"images/08-warlord.jpg",
+		// 		"images/09-10-paladin.jpg",
+		// 		"images/09-10-priestess.jpg",
+		// 		"images/09-10-templar.jpg",
+		// 		"images/11-12-baker.jpg",
+		// 		"images/11-12-butcher.jpg",
+		// 		"images/11-12-miner.jpg",
+		// 		"images/grass-tile.jpg",
+		// 		"images/icon.jpg",
+		// 		"images/monster-barrens.jpg",
+		// 		"images/monster-cavern.jpg",
+		// 		"images/monster-desert.jpg",
+		// 		"images/monster-forest.jpg",
+		// 		"images/monster-glacier.jpg",
+		// 		"images/monster-hills.jpg",
+		// 		"images/monster-mountains.jpg",
+		// 		"images/monster-oasis.jpg",
+		// 		"images/monster-ruins.jpg",
+		// 		"images/monster-swamp.jpg",
+		// 		"images/monster-tundra.jpg",
+		// 		"images/monster-undead-samurai.jpg",
+		// 		"images/monster-valley.jpg",
+		// 		"images/monster-volcano.jpg",
+		// 		"dist/build.js",
+		// 		"dist/grass-tile.jpg",
+		// 	],
+		// 	network: ["*"],
+		// 	exclude: [ /.*\.appcache/ ],
+		// 	output: "manifest.appcache",
+		// }),
 	])
 }
